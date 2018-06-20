f3fd30d2fb9f364ff60adc16f189996a363a2543
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-06-20 12:23:37 +0000

    Done.

---
+++ naenae.rb
@@ -0,0 +1,14 @@
+loop do
+  puts "Now watch me whip"
+  puts "Watch me nae nae"
+  puts "Now watch me whip, whip"
+  puts "Now watch me nae nae"
+  puts "Oooh, watch me, watch me"
+  puts "Oooh, watch me, watch me"
+  puts "Oooh, watch me, watch me"
+  break 
+end
+
+puts "Do the stanky leg"
+puts "Do the stanky leg"
+exit
